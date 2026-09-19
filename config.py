886a36f911f9702000b7fc253be0b056fb6de144
Eduardo Mello da Costa <mellodacostaeduardo@gmail.com>
2026-09-19 15:43:53 -0300

    Configure real player competitions

--- config.py
+++ config.py
@@ -6,11 +6,12 @@ class League:
     name: str
     country: str
     espn_slug: str
+    sofascore_tournament_id: int
 
 LEAGUES = {
-    "bra_a": League("bra_a", "Brasileirão Série A", "Brasil", "bra.1"),
-    "bra_b": League("bra_b", "Brasileirão Série B", "Brasil", "bra.2"),
-    "arg_a": League("arg_a", "Liga Profesional Argentina", "Argentina", "arg.1"),
+    "bra_a": League("bra_a", "Brasileirão Série A", "Brasil", "bra.1", 325),
+    "bra_b": League("bra_b", "Brasileirão Série B", "Brasil", "bra.2", 390),
+    "arg_a": League("arg_a", "Liga Profesional Argentina", "Argentina", "arg.1", 155),
 }
 
 DEFAULT_SEASON = 2026
